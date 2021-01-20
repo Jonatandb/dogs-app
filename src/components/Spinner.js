943b6ad926672cf8103bdc6b3651cc5c1bb9bfd1
Jonatandb <jonatandb@gmail.com>
2021-01-20 01:05:23 -0300

    React App ✅ DOG API - Con Manejo de Errores
    https://www.youtube.com/watch?v=pbAeTikKCmQ&ab_channel=LuisCabrera

---
+++ src/components/Spinner.js
@@ -0,0 +1,17 @@
+// https://loading.io/css
+import React from 'react'
+
+import './Spinner.css'
+
+const Spinner = () => {
+    return (
+        <div className='lds-ellipsis'>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    )
+}
+
+export default Spinner
